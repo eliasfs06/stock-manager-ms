a94b50ccf65ac9d9366779f090edf649f9fd9911
Elias Silva <eliasfs0612@gmail.com>
2024-08-04 22:41:39 -0300

    add gateway and eureka

---
+++ gateway/src/test/java/com/eliasfs06/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliasfs06.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
